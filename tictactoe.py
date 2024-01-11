7a635d9eddd4bc53e307e8d71581ab0f2ffa584e
Thomas Caron <tc.caron91@gmail.com>
2024-01-11 10:30:18 +0100

    Added comments

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,14 @@
 import csv
 import random
 
-
+# Classe pour le jeu Tic Tac Toe
 class TicTacToeGame:
     def __init__(self):
-        self.state = '         '
-        self.player = 'X'
-        self.winner = None
+        self.state = '         '  # État initial du plateau de jeu
+        self.player = 'X'  # Joueur actuel (X ou O)
+        self.winner = None  # Gagnant du jeu
 
+    # Retourne les mouvements possibles dans l'état actuel
     def allowed_moves(self):
         states = []
         for i in range(len(self.state)):
@@ -15,6 +16,7 @@ class TicTacToeGame:
                 states.append(self.state[:i] + self.player + self.state[i + 1:])
         return states
 
+    # Effectue un mouvement et met à jour l'état du jeu
     def make_move(self, next_state):
         if self.winner:
             raise (Exception("Game already completed, cannot make another move!"))
@@ -31,9 +33,11 @@ class TicTacToeGame:
         else:
             self.player = 'X'
 
+    # Vérifie si le jeu est encore jouable
     def playable(self):
         return (not self.winner) and any(self.allowed_moves())
 
+    # Prédit le gagnant du jeu
     def predict_winner(self, state):
         lines = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
         winner = None
@@ -45,12 +49,14 @@ class TicTacToeGame:
                 winner = 'O'
         return winner
 
+    # Vérifie si le mouvement est valide
     def __valid_move(self, next_state):
         allowed_moves = self.allowed_moves()
         if any(state == next_state for state in allowed_moves):
             return True
         return False
 
+    # Affiche le plateau de jeu
     def print_board(self):
         s = self.state
         print('     {} | {} | {} '.format(s[0], s[1], s[2]))
@@ -59,21 +65,22 @@ class TicTacToeGame:
         print('    -----------')
         print('     {} | {} | {} '.format(s[6], s[7], s[8]))
 
-
+# Classe pour l'agent d'apprentissage par renforcement Q-Learning
 class QLearningAgent:
     def __init__(self, game_class, epsilon=0.1, alpha=0.5, gamma=0.9, value_player='X'):
-        # Init Q-Table
-        self.Q = dict()
-        self.NewGame = game_class
-        self.epsilon = epsilon
-        self.alpha = alpha
-        self.gamma = gamma
-        self.value_player = value_player
+        self.Q = dict()  # Table Q pour stocker les valeurs
+        self.NewGame = game_class  # Classe de jeu
+        self.epsilon = epsilon  # Taux d'exploration
+        self.alpha = alpha  # Taux d'apprentissage
+        self.gamma = gamma  # Facteur de remise
+        self.value_player = value_player  # Joueur pour lequel on calcule la valeur
 
+    # Apprend à jouer au jeu sur un certain nombre d'épisodes
     def learn_game(self, num_episodes=1000):
         for episode in range(num_episodes):
             self.learn_from_episode()
 
+    # Apprend à partir d'un seul épisode
     def learn_from_episode(self):
         game = self.NewGame()
         state = game.state
@@ -83,6 +90,7 @@ class QLearningAgent:
             self.update_q_table(state, action, reward, next_state)
             state = next_state
 
+    # Sélectionne une action en fonction de la politique epsilon-greedy
     def select_action(self, state, allowed_moves):
         if random.random() < self.epsilon:
             return random.choice(allowed_moves)
@@ -92,19 +100,23 @@ class QLearningAgent:
             max_q_actions = [action for action, q in zip(allowed_moves, q_values) if q == max_q]
             return random.choice(max_q_actions)
 
+    # Effectue un mouvement et retourne la récompense
     def make_move_and_get_reward(self, game, action):
         game.make_move(action)
         reward = self.__reward(game)
         return reward, game.state
 
+    # Met à jour la table Q
     def update_q_table(self, state, action, reward, next_state):
         current_q = self.get_q_value(state, action)
         next_max_q = max([self.get_q_value(next_state, a) for a in self.NewGame().allowed_moves()], default=0)
         self.Q[(state, action)] = current_q + self.alpha * (reward + self.gamma * next_max_q - current_q)
 
+    # Obtient la valeur Q pour un état et une action donnés
     def get_q_value(self, state, action):
         return self.Q.get((state, action), 0.0)
 
+    # Calcule la récompense
     def __reward(self, game):
         if game.winner == self.value_player:
             return 1.0
@@ -113,13 +125,15 @@ class QLearningAgent:
         else:
             return 0.0
 
+    # Sauvegarde la table Q dans un fichier CSV
     def save_q_table(self):
         with open('q_table.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['State before win', 'Last action', 'Reward'])
+            writer.writerow(['State before win', 'Win action', 'Reward'])
             for (state, action), value in self.Q.items():
                 writer.writerow([state, action, value])
 
+    # Démontre un jeu en utilisant la meilleure stratégie
     def demo_game(self, verbose=False):
         game = self.NewGame()
         t = 0
@@ -142,6 +156,7 @@ class QLearningAgent:
                 print("\nIt's a draw!")
             return '-'
 
+    # Sélectionne le meilleur mouvement en fonction de la table Q
     def select_best_move(self, game):
         state = game.state
         allowed_moves = game.allowed_moves()
@@ -150,13 +165,13 @@ class QLearningAgent:
         best_actions = [action for action, q in zip(allowed_moves, q_values) if q == max_q]
         return random.choice(best_actions)
 
-
+# Fonction pour afficher les statistiques d'un jeu de démonstration
 def demo_game_stats(agent):
     results = [agent.demo_game() for i in range(10000)]
     game_stats = {k: results.count(k) / 100 for k in ['X', 'O', '-']}
     print("    percentage results: {}".format(game_stats))
 
-
+# Point d'entrée principal
 if __name__ == '__main__':
     agent = QLearningAgent(TicTacToeGame, epsilon=0.1, alpha=1.0, gamma=0.9)
     print("Before learning:")
